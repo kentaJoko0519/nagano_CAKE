c50fa0870a43ae843db285bc5a1e275b7526dbc9
KentaJoko <126621899+kentaJoko0519@users.noreply.github.com>
2023-04-10 04:26:39 +0000

    [adminのCVとルーティング作成]応用課題

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   
+  namespace :admin do
+    get 'orders/show'
+  end
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
@@ -27,5 +30,15 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     get "/orders/complete"=>"orsers#complete", as: 'complete'
     resources :addresses, only: [:create, :index, :edit, :update, :destroy]
   end  
+  
+  # 管理者用
+  namespace :admin do
+    resources :homes, only: [:top]
+    resources :items, only: [:index, :show, :create, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show,:update]
+    resources :order_details, only: [:update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
