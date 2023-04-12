a39c3143fb87c99ce6f1b948c6d5bb18528a4493
KentaJoko <126621899+kentaJoko0519@users.noreply.github.com>
2023-04-12 07:13:03 +0000

    [管理者：ジャンル一覧、追加、編集機能　追加]応用課題

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -25,6 +25,9 @@ class Admin::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
   
+  def after_sign_in_path_for(resource)
+    admin_items_path
+  end
   
   #protected
     # 退会しているかを判断するメソッド
