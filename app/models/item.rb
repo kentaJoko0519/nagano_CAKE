78c445c83cd71a564dc8086b7ab3383ba810b73e
KentaJoko <126621899+kentaJoko0519@users.noreply.github.com>
2023-04-13 11:16:17 +0000

    [adminの商品一覧＆商品新規登録画面]応用課題

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,9 @@
 class Item < ApplicationRecord
-  
+
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
   belongs_to :genre
-  
+
   has_one_attached :item_image
 
   def get_item_image(width, height)
@@ -13,5 +13,5 @@ class Item < ApplicationRecord
     end
     item_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
 end
