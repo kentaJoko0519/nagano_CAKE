979f50efceadf9c4e1a1633cf78281c658e7d2f4
KentaJoko <126621899+kentaJoko0519@users.noreply.github.com>
2023-04-17 13:06:13 +0000

    [ECサイト：マイページ、会員情報編集、退会ページ作成]応用課題

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,7 +2,6 @@
 
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
   # GET /resource/sign_in
   # def new
   #   super
@@ -29,20 +28,5 @@ class Admin::SessionsController < Devise::SessionsController
     admin_root_path
   end
 
-  #protected
-    # 退会しているかを判断するメソッド
-  #def customer_state
-    ## 【処理内容1】 入力されたemailからアカウントを1件取得
-    #@customer = Customer.find_by(email: params[:customer][:email])
-    ## アカウントを取得できなかった場合、このメソッドを終了する
-    #return if !@customer
-    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
-    #if @customer.valid_password?(params[:customer][:password])
-    ## 【処理内容3】
-    #elsif @customer.is_deieted == true && !false
-      #redirect_to new_customer_session_path
-    #else
-      #redirect_to customer_session_path
-    #end
-  #end
+  
 end
