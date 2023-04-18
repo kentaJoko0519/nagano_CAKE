96368bfea817f6fdbf200067a08186c928b1bd16
KentaJoko <126621899+kentaJoko0519@users.noreply.github.com>
2023-04-18 05:41:02 +0000

    [管理者：会員一覧、詳細、編集ページ作成]応用課題

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -26,6 +26,8 @@ class Public::CustomersController < ApplicationController
     redirect_to root_path
   end
   
+  private
+  
   def customer_params
     params.require(:customer).permit(:first_name,:last_name,:first_name_kana,:last_name_kana,:postal_code,:address,:telephone_number,:email)
   end
