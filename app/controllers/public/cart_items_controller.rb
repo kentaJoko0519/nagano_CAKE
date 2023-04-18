d03495d967d0975f53eb0a5bc0b8c9af6ec8b60c
KentaJoko <126621899+kentaJoko0519@users.noreply.github.com>
2023-04-18 09:33:05 +0000

    [ECサイト：商品一覧、詳細ページ作成]応用課題

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,17 +1,26 @@
 class Public::CartItemsController < ApplicationController
   def index
   end
-  
+
   def create
+    @cart_item=CartItem.new(cart_item_params)
+    @cart_item.save
+    redirect_to cart_items_path
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
   def destroy_all
   end
-  
+
+    private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:amount,:item_id,:customer_id)
+  end
+
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,20 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items=Item.page(params[:page])
   end
 
   def show
+    @item=Item.find(params[:id])
+    # cart_itemのコントローラーに受け渡す
+    @cart_item=CartItem.new
+    # cart_itemのコントローラーに顧客のID（customer_id）を送るため
+    @customer=current_customer
   end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:item_image, :name, :genre_id,:introduction,:price,:is_active)
+  end
+  
 end
