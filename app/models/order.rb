384d1dbab8338218e86b88685a179b8ebd44279d
KentaJoko <126621899+kentaJoko0519@users.noreply.github.com>
2023-04-27 11:26:24 +0000

    [管理者：注文履歴、注文詳細作成中]応用課題

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,8 @@
 class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { waiting: 0, confirmation: 1, production:2, preparing_delivery:3, delivered:4 }
 
   belongs_to :customer
   has_many :order_details, dependent: :destroy
-
+  
 end
