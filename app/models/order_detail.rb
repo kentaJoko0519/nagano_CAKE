049c2cef0aa9c6220a751c3d1d33be1cf5d721ba
KentaJoko <126621899+kentaJoko0519@users.noreply.github.com>
2023-04-28 01:06:10 +0000

    [管理者：注文詳細作成中]応用課題

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,6 +1,12 @@
 class OrderDetail < ApplicationRecord
   
+  enum making_status: { not_possible: 0, pending: 1, making: 2, complete: 3 }
+  
   belongs_to :order
   belongs_to :item
+  
+  def subtotal
+    item.with_tax_price * amount
+  end
   
 end
